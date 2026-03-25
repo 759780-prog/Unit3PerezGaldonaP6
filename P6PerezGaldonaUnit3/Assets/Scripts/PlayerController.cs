b5dda69163cb3e239c8332eee7fc92e1d34925cd
759780-prog <759780@student.dusd.net>
2026-03-25 13:48:12 -0700

    almost done with unit 3 project
    half way through unit 3 project.

---
+++ P6PerezGaldonaUnit3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Rigidbody playerRb;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 500);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 100, ForceMode.Impulse);
+
+
+
+        }
+    }
+}
+
